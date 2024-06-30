a7c0d6f4ac6926fa6cd5c0969d37e2c6af092010
mbabi <mbabiak39@gmail.com>
2024-06-30 16:02:12 +0200

    add stop vm function

--- cloudfunction/main.py
+++ cloudfunction/main.py
@@ -3,7 +3,11 @@
 # Assumes a table call resources
 # resources schema - single column:  message:string
 import base64
+import json
+from collections import defaultdict
 from google.cloud import bigquery
+from google.cloud import compute_v1
+
 table_id = "activities.resources"
 
 
@@ -12,7 +16,30 @@ def pubsub_to_bigquery(event, context):
     print("Row To Insert: " + pubsub_message)
     client = bigquery.Client()
     table = client.get_table(table_id)
-    row_to_insert = [(pubsub_message,)]     # NOTE - the trailing comma is required for this case - it expects a tuple
+    row_to_insert = [(pubsub_message,)]  # NOTE - the trailing comma is required for this case - it expects a tuple
     errors = client.insert_rows(table, row_to_insert)
     if errors == []:
         print("Row Inserted: " + pubsub_message)
+
+
+def stop_vm(event, context):
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    message_data = json.loads(pubsub_message)
+    instance_client = compute_v1.InstancesClient()
+    instance_name = message_data['incident']['metric']['labels']['instance_name']
+    project_id = message_data['incident']['resource']['labels']['project_id']
+    zone = find_instance_zone(project_id=project_id, instance_name=instance_name, client=instance_client)
+
+    ops = instance_client.stop(project_id=project_id, zone=zone, instance=instance_name)
+    ops_result = ops.result()
+
+
+def find_instance_zone(project_id: str, instance_name: str, client) -> str:
+    instances = client.list(project=project_id)
+
+    for instance in instances:
+        if instance.name == instance_name:
+            return instance.zone.split('/')[-1]  # Extracts the zone name from the full path
+
+    # If instance not found in any zone
+    return ""
